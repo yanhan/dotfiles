1ba8dc312d261955a5d557fb6567b275b27d4ada
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2020-12-25 19:00:43 +0800

    [FEAT] Support init.vim (which is not a dotfile)
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- setup_home_folder_dotfiles.py
+++ setup_home_folder_dotfiles.py
@@ -9,9 +9,35 @@ import re
 import shutil
 import sys
 
-VALID_DOTFILES = set(["bashrc", "gitconfig", "tmux.conf", "vimrc", "zshrc",])
-TEMPLATE_FOLDER = "templates"
 HOME_FOLDER = os.environ["HOME"]
+
+class ConfigFile:
+  def __init__(self, directory, filename):
+    self.directory = directory
+    self.filename = filename
+
+  def get_path(self):
+      return os.path.join(self.directory, self.filename)
+
+  def get_backup_path(self):
+      filename = self.filename
+      if filename[0] == ".":
+          filename = filename[1:]
+      return os.path.join(self.directory, "{}.bak".format(filename))
+
+VALID_DOTFILES = {
+  "bashrc": ConfigFile(HOME_FOLDER, ".bashrc"),
+  "gitconfig": ConfigFile(HOME_FOLDER, ".gitconfig"),
+  "init.vim": ConfigFile(
+    os.path.join(HOME_FOLDER, ".config", "nvim"),
+    "init.vim",
+  ),
+  "tmux.conf": ConfigFile(HOME_FOLDER, ".tmux.conf"),
+  "vimrc": ConfigFile(HOME_FOLDER, ".vimrc"),
+  "zshrc": ConfigFile(HOME_FOLDER, ".zshrc"),
+}
+
+TEMPLATE_FOLDER = "templates"
 BAK_FILE_REGEX = re.compile(r"""\.bak\.(\d+)$""")
 
 def _get_sha256sum_of_file(file_name):
@@ -23,10 +49,9 @@ def _get_sha256sum_of_file(file_name):
   else:
     return None
 
-def _construct_home_folder_dotfile_backup_file_name(dotfile_name):
-  bak_file_list = glob.glob("{}.bak*".format(
-    os.path.join(HOME_FOLDER, dotfile_name)
-  ))
+def _get_dotfile_backup_path(file_info):
+  backup_path = file_info.get_backup_path()
+  bak_file_list = glob.glob("{}*".format(backup_path))
   print(bak_file_list)
   digit_list = []
   for f in bak_file_list:
@@ -39,36 +64,28 @@ def _construct_home_folder_dotfile_backup_file_name(dotfile_name):
     bak_file_num_string = ".{}".format(str(max(digit_list) + 1))
   elif bak_file_list:
     bak_file_num_string = ".1"
-  return os.path.join(HOME_FOLDER,
-    "{}.bak{}".format(dotfile_name, bak_file_num_string)
-  )
+  return "{}{}".format(backup_path, bak_file_num_string)
 
 def _setup_dotfile(dotfile_name):
+  global VALID_DOTFILES, TEMPLATE_FOLDER
+  file_info = VALID_DOTFILES[dotfile_name]
+  dest_dir = file_info.directory
   template_dotfile_path = os.path.join(TEMPLATE_FOLDER, dotfile_name)
   template_dotfile_sha256sum = _get_sha256sum_of_file(template_dotfile_path)
-  home_folder_dotfile_path = os.path.join(HOME_FOLDER,
-    ".{}".format(dotfile_name)
-  )
-  home_folder_dotfile_sha256sum = _get_sha256sum_of_file(
-    home_folder_dotfile_path
-  )
-  if home_folder_dotfile_sha256sum is not None and \
-      home_folder_dotfile_sha256sum != template_dotfile_sha256sum:
+  dest_dotfile = file_info.get_path()
+  dest_dotfile_sha256sum = _get_sha256sum_of_file(dest_dotfile)
+  if dest_dotfile_sha256sum is not None and \
+      dest_dotfile_sha256sum != template_dotfile_sha256sum:
     # backup original dotfile
-    home_folder_dotfile_backup_path = \
-      _construct_home_folder_dotfile_backup_file_name(dotfile_name)
-    shutil.move(home_folder_dotfile_path, home_folder_dotfile_backup_path)
-    shutil.copyfile(template_dotfile_path, home_folder_dotfile_path)
-    print("Backing up {} to {}".format(home_folder_dotfile_path,
-      home_folder_dotfile_backup_path
-    ))
+    dotfile_backup_path = _get_dotfile_backup_path(file_info)
+    shutil.move(dest_dotfile, dotfile_backup_path)
+    shutil.copyfile(template_dotfile_path, dest_dotfile)
+    print("Backing up {} to {}".format(dest_dotfile, dotfile_backup_path))
+    print("Copying template {} to {}".format(dotfile_name, dest_dotfile))
+  elif dest_dotfile_sha256sum is None:
+    shutil.copyfile(template_dotfile_path, dest_dotfile)
     print("Copying template {} to {}".format(dotfile_name,
-      home_folder_dotfile_path
-    ))
-  elif home_folder_dotfile_sha256sum is None:
-    shutil.copyfile(template_dotfile_path, home_folder_dotfile_path)
-    print("Copying template {} to {}".format(dotfile_name,
-      home_folder_dotfile_path
+      dest_dotfile
     ))
 
 def _main(dotfiles_list):
