ce61481596dee8a34aea7863876186a72bc4be35
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2020-12-29 08:44:03 +0800

    [FEAT] Add install-nvr.sh script for Mac
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

---
+++ install-nvr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+main() {
+	if [ "$(uname)" != "Darwin" ]; then
+		printf "Please use the provision-ubuntu-ansible repo to install neovim-remote\n"
+		printf "Exiting.\n"
+		exit 1
+	fi
+	set +u
+	eval "$(pyenv init -)"
+	set -u
+
+	declare -r python_version=3.9.1
+	local lines_found
+
+	set +e
+	lines_found="$(pyenv versions | grep "${python_version}" | wc -l)"
+	set -e
+	if [ "${lines_found}" -eq 0 ]; then
+		pushd ~/.pyenv
+		git fetch origin
+		git merge --ff-only origin/master
+		pyenv install "${python_version}"
+		pyenv global "${python_version}"
+		popd
+	fi
+
+	set +e
+	lines_found="$(pip3 list 2>/dev/null | grep neovim-remote | wc -l)"
+	set -e
+	if [ "${lines_found}" -eq 0 ]; then
+		pip3 install --user neovim-remote
+	fi
+}
+
+main
