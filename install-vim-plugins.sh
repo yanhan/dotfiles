23733c7ab78f3a2ba7dc8b7c8927711007afb4a4
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2022-08-30 16:30:32 +0800

    [FIX] Use https URLs for git repos
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -7,68 +7,68 @@ if [ ! -d "${PACKAGE_PATH}" ]; then
 fi
 
 if [ ! -d ~/.fzf ]; then
-	git clone git://github.com/junegunn/fzf.git ~/.fzf
+	git clone https://github.com/junegunn/fzf.git ~/.fzf
 fi
 ~/.fzf/install
 
 pushd "${PACKAGE_PATH}"
 
 if [ ! -d molokai ]; then
-	git clone git://github.com/tomasr/molokai.git
+	git clone https://github.com/tomasr/molokai.git
 fi
 
 if [ ! -d nerdcommenter ]; then
-	git clone git://github.com/preservim/nerdcommenter.git
+	git clone https://github.com/preservim/nerdcommenter.git
 fi
 
 if [ ! -d nerdtree ]; then
-	git clone git://github.com/preservim/nerdtree.git
+	git clone https://github.com/preservim/nerdtree.git
 fi
 
 if [ ! -d vim-textobj-user ]; then
-	git clone git://github.com/kana/vim-textobj-user.git
+	git clone https://github.com/kana/vim-textobj-user.git
 fi
 
 if [ ! -d vim-textobj-entire ]; then
-	git clone git://github.com/kana/vim-textobj-entire.git
+	git clone https://github.com/kana/vim-textobj-entire.git
 fi
 
 if [ ! -d vim-surround ]; then
-	git clone git://github.com/tpope/vim-surround.git
+	git clone https://github.com/tpope/vim-surround.git
 fi
 
 if [ ! -d vim-closetag ]; then
-	git clone git://github.com/alvan/vim-closetag.git
+	git clone https://github.com/alvan/vim-closetag.git
 fi
 
 if [ ! -d vim-go ]; then
-	git clone git://github.com/fatih/vim-go
+	git clone https://github.com/fatih/vim-go
 fi
 
 if [ ! -d fzf.vim ]; then
-	git clone git://github.com/junegunn/fzf.vim
+	git clone https://github.com/junegunn/fzf.vim
 fi
 
 if [ ! -d vim-projectionist ]; then
-	git clone git://github.com/tpope/vim-projectionist.git
+	git clone https://github.com/tpope/vim-projectionist.git
 fi
 
 if [ ! -d vim-obsession ]; then
-	git clone git://github.com/tpope/vim-obsession.git
+	git clone https://github.com/tpope/vim-obsession.git
 fi
 
 if [ ! -d ale ]; then
-	git clone git@github.com:dense-analysis/ale.git
+	git clone https://github.com/dense-analysis/ale.git
 fi
 
 if [ ! -d editorconfig-vim ]; then
-	git clone git@github.com:editorconfig/editorconfig-vim.git
+	git clone https://github.com/editorconfig/editorconfig-vim.git
 fi
 
 ### Mac specific
 
 if [ ! -d vim-colors-solarized ]; then
-	git clone git://github.com/altercation/vim-colors-solarized.git
+	git clone https://github.com/altercation/vim-colors-solarized.git
 fi
 
 popd
