d2297c707010032a19aca0fa814a95d308ce759d
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2022-09-06 11:41:03 +0800

    [R] vim plugins: install each in own dir
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -11,89 +11,75 @@ if [ ! -d ~/.fzf ]; then
 fi
 ~/.fzf/install
 
+readonly NEWLY_CLONED='NEWLY CLONED'
+
+clone_plugin_repo() {
+  declare -r plugin_name=${1}
+  shift
+  declare -r repo_url=${1}
+  shift
+  declare -r plugin_dir="${plugin_name}/start"
+  if [ ! -d "${plugin_dir}" ]; then
+    mkdir -pv "${plugin_dir}"
+    pushd "${plugin_dir}"
+    set -x
+    git clone "$@" "${repo_url}"
+    set +x
+    popd
+    echo "${NEWLY_CLONED}"
+  fi
+}
+
+install_vim_go() {
+  local output
+  output="$(clone_plugin_repo vim-go https://github.com/fatih/vim-go.git)"
+  output="$(tail -1 <<< "${output}")"
+  if [ "${output}" = "${NEWLY_CLONED}" ]; then
+    nvim -c 'GoInstallBinaries' -c q
+  fi
+}
+
+install_coc_nvim() {
+  local output
+  output="$(clone_plugin_repo coc.nvim https://github.com/neoclide/coc.nvim.git --branch release --depth 1)"
+  output=$(tail -1 <<< "${output}")
+  if [ "${output}" = "${NEWLY_CLONED}" ]; then
+    pushd "${PACK_PATH}/coc.nvim/start"
+    nvim -c 'helptags coc.nvim/doc/ | q'
+    popd
+  fi
+}
+
+install_jedi_vim() {
+  local output
+  output="$(clone_plugin_repo jedi-vim https://github.com/davidhalter/jedi-vim.git)"
+  output="$(tail -1 <<< "${output}")"
+  if [ "${output}" = "${NEWLY_CLONED}" ]; then
+    pushd "${PACK_PATH}/jedi-vim/start/jedi-vim"
+    git submodule update --init --recursive
+    nvim -c 'CocInstall coc-jedi' -c q
+    popd
+  fi
+}
+
 pushd "${PACK_PATH}"
-readonly molokai_dir=molokai/start
-if [ ! -d "${molokai_dir}" ]; then
-  mkdir -pv "${molokai_dir}"
-  pushd "${molokai_dir}"
-  git clone https://github.com/tomasr/molokai.git
-  popd
-fi
-popd
-
-pushd "${PACKAGE_PATH}"
-
-if [ ! -d nerdcommenter ]; then
-	git clone https://github.com/preservim/nerdcommenter.git
-fi
-
-if [ ! -d nerdtree ]; then
-	git clone https://github.com/preservim/nerdtree.git
-fi
-
-if [ ! -d vim-textobj-user ]; then
-	git clone https://github.com/kana/vim-textobj-user.git
-fi
-
-if [ ! -d vim-textobj-entire ]; then
-	git clone https://github.com/kana/vim-textobj-entire.git
-fi
-
-if [ ! -d vim-surround ]; then
-	git clone https://github.com/tpope/vim-surround.git
-fi
-
-if [ ! -d vim-closetag ]; then
-	git clone https://github.com/alvan/vim-closetag.git
-fi
-
-if [ ! -d vim-go ]; then
-	git clone https://github.com/fatih/vim-go
-	nvim -c 'GoInstallBinaries' -c q
-fi
-
-if [ ! -d fzf.vim ]; then
-	git clone https://github.com/junegunn/fzf.vim
-fi
-
-if [ ! -d vim-projectionist ]; then
-	git clone https://github.com/tpope/vim-projectionist.git
-fi
-
-if [ ! -d vim-obsession ]; then
-	git clone https://github.com/tpope/vim-obsession.git
-fi
-
-if [ ! -d ale ]; then
-	git clone https://github.com/dense-analysis/ale.git
-fi
-
-if [ ! -d editorconfig-vim ]; then
-	git clone https://github.com/editorconfig/editorconfig-vim.git
-fi
-
-if [ ! -d coc.nvim ]; then
-  git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1
-  # Required for generating coc help pages
-  nvim -c 'helptags coc.nvim/doc/ | q'
-fi
-
-if [ ! -d vim-airline ]; then
-  git clone https://github.com/vim-airline/vim-airline.git
-fi
-
-if [ ! -d jedi-vim ]; then
-  git clone https://github.com/davidhalter/jedi-vim.git
-  pushd jedi-vim
-  git submodule update --init --recursive
-  nvim -c 'CocInstall coc-jedi' -c q
-  popd
-fi
+clone_plugin_repo molokai https://github.com/tomasr/molokai.git
+clone_plugin_repo nerdcommenter https://github.com/preservim/nerdcommenter.git
+clone_plugin_repo nerdtree https://github.com/preservim/nerdtree.git
+clone_plugin_repo vim-textobj-user https://github.com/kana/vim-textobj-user.git
+clone_plugin_repo vim-textobj-entire https://github.com/kana/vim-textobj-entire.git
+clone_plugin_repo vim-surround https://github.com/tpope/vim-surround.git
+clone_plugin_repo vim-closetag https://github.com/alvan/vim-closetag.git
+install_vim_go
+clone_plugin_repo fzf.vim https://github.com/junegunn/fzf.vim
+clone_plugin_repo vim-projectionist https://github.com/tpope/vim-projectionist.git
+clone_plugin_repo vim-obsession https://github.com/tpope/vim-obsession.git
+clone_plugin_repo ale https://github.com/dense-analysis/ale.git
+clone_plugin_repo editorconfig-vim https://github.com/editorconfig/editorconfig-vim.git
+clone_plugin_repo vim-airline https://github.com/vim-airline/vim-airline.git
+install_coc_nvim
+install_jedi_vim
 
 ### Mac specific
-
-if [ ! -d vim-colors-solarized ]; then
-	git clone https://github.com/altercation/vim-colors-solarized.git
-fi
-
+clone_plugin_repo vim-colors-solarized https://github.com/altercation/vim-colors-solarized.git
 popd
