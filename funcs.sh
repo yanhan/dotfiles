13ef22e1544856e5f64c355be90226689655ca73
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2022-09-18 14:12:36 +0800

    [FEAT] Move new-bash-script from bin-src to funcs.sh
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- funcs.sh
+++ funcs.sh
@@ -4,3 +4,31 @@
 b64d() {
   base64 -d <<< "${1}"; echo
 }
+
+new_bash_script() {
+  if [ "${#}" -ne 1 ]; then
+    printf >&2 "Usage: %s <filename>\n" "${funcstack}"
+    return 1
+  fi
+
+  if [ -f "${1}" ]; then
+    printf >&2 "Error: file/dir '%s' exists. Please run with another filename, or remove the original file and try again.\n" "${1}"
+    return 1
+  fi
+
+  cat >"${1}" <<EOF
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+main() {
+}
+
+main "\$@"
+EOF
+
+  chmod "${1}"
+  # Open the script and shift cursor to line 6
+  v +6 "${1}"
+}
