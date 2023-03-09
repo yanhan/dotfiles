b32cb1153db6440b5d8d2d3752c7898c93c0a2c1
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2023-03-09 21:04:03 +0800

    [FEAT] Change number of characters to trigger autocompletion
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- lsp.lua
+++ lsp.lua
@@ -5,6 +5,27 @@ local lsp = require("lsp-zero").preset({
   suggest_lsp_servers = false,
 })
 
+-- Change number of characters required to trigger autocompletion
+-- Sources:
+-- https://github.com/VonHeikemen/lsp-zero.nvim/issues/131
+-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/07e43a593241bce72c0854398b16f51e0b46486e/advance-usage.md#setting-up-sources
+lsp.setup_nvim_cmp({
+  sources = {
+    {name = "path"},
+    -- Require 2 characters to trigger autocompletion using LSP
+    {name = "nvim_lsp", keyword_length = 2},
+    -- Only require 1 character to trigger autocompletion using words in
+    -- the current buffer
+    {name = "buffer", keyword_length = 1},
+    -- Still trying to figure this one out or how to use it.
+    -- Some help:
+    -- https://github.com/L3MON4D3/LuaSnip
+    -- https://github.com/L3MON4D3/LuaSnip/blob/b5a72f1fbde545be101fcd10b70bcd51ea4367de/Examples/snippets.lua
+    -- https://jdhao.github.io/2020/05/27/why_you_should_use_snippets_in_vim/
+    {name = "luasnip", keyword_length = 3},
+  }
+})
+
 -- Names are from: https://github.com/williamboman/mason-lspconfig.nvim
 -- What the :Mason command provides is inaccurate
 lsp.ensure_installed({
