f46d596e8f867df65b296bfda7fd1dee62cc17e4
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2025-10-12 11:07:50 +0800

    Add indentation settings for java files
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- nvim-lua/my/autocommand.lua
+++ nvim-lua/my/autocommand.lua
@@ -4,6 +4,20 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
   command = "!pdflatex <afile>"
 })
 
+-- Based on code in https://github.com/FotiadisM/tabset.nvim/commit/996f95e4105d053a163437e19a40bd2ea10abeb2
+-- We should probably just use that library
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("tabset", { clear = true }),
+  callback = function()
+    if vim.o.filetype == "java" then
+      vim.o.tabstop = 4
+      vim.o.shiftwidth = 4
+      vim.o.softtabstop = 4
+      vim.o.expandtab = true
+    end
+  end
+})
+
 -- Neither of the below work
 -- Source: https://github.com/mhinz/neovim-remote/tree/1004d41696a3de12f0911b1949327c3dbe4a62ab
 --vim.cmd [[ autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete ]]
