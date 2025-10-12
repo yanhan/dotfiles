cadcafb57e2ac6f4111b78e301524c345d4df0da
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2025-10-12 12:14:04 +0800

    neovim: setup lua lsp
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -1,4 +1,8 @@
 -- This should be in the ~/.config/nvim dir
 require("my")
 
+-- Prevent builtin vim.lsp.completion autotrigger from selecting 1st item
+-- Source: https://blog.viktomas.com/graph/neovim-native-built-in-lsp-autocomplete/
+vim.opt.completeopt = {"menuone", "noselect", "popup"}
+
 vim.lsp.enable('luals')
--- nvim-lua/lsp/luals.lua
+++ nvim-lua/lsp/luals.lua
@@ -1,3 +1,11 @@
+-- Lua LSP configuration
+-- Note that this requires that the lua-language-server binary is installed.
+-- That can be done using asdf: https://github.com/bellini666/asdf-lua-language-server
+--
+-- The configuration is a combination of:
+-- https://vonheikemen.github.io/learn-nvim/feature/lsp-setup.html
+-- https://luals.github.io/wiki/configuration/#neovim
+-- https://blog.viktomas.com/graph/neovim-native-built-in-lsp-autocomplete/
 return {
   cmd = {'lua-language-server'},
   filetypes = {'lua'},
@@ -8,5 +16,14 @@ return {
         version = "LuaJIT"
       }
     }
-  }
+  },
+  on_attach = function(client, bufnr)
+    vim.lsp.completion.enable(true, client.id, bufnr, {
+      autotrigger = true,
+      convert = function(item)
+        return { abbr = item.label:gsub("%b()", "") }
+      end,
+    })
+    vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+  end
 }
